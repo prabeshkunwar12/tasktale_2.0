70b80954a1e827bf616155b60ad4b8801cade6f9
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-02-24 14:47:13 -0400

    next auth installed and setup

--- actions/login.ts
+++ actions/login.ts
@@ -1,14 +1,26 @@
 'use server'
 
+import { getUserByEmail } from "@/lib/data/user"
 import { LoginSchema } from "@/schemas"
+import bcrypt from 'bcryptjs'
 import * as z from 'zod'
 
 export const login = async (values:z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values)
 
-    if(!validatedFields) {
+    if(!validatedFields.success) {
         return {error: "Invalid Fields!"}
     }
+
+    const { email, password } = validatedFields.data
+
+    const existingUser = await getUserByEmail(email)
+
+    if(!existingUser ?? existingUser?.password) {
+        return {error: "Invalid credentials!"}
+    }
+
+    const passwordMatch = bcrypt.compare(password, existingUser?.password as string)
 
     return {success: "Email Sent"}
 }
