30c69590d1dfd6ffbef94aef163024ce2a9a62b0
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-02-24 14:13:11 -0400

    register action functional

--- actions/register.ts
+++ actions/register.ts
@@ -2,13 +2,36 @@
 
 import { RegisterSchema } from "@/schemas"
 import * as z from 'zod'
+import bcrypt from 'bcryptjs'
+import db from "@/lib/db"
 
 export const register = async (values:z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
 
-    if(!validatedFields) {
+    if(!validatedFields.success) {
         return {error: "Invalid Fields!"}
     }
+
+    const { name, email, password } = validatedFields.data
+    const hashedPassword  = await bcrypt.hash(password, 10)
+
+    const existingUser = await db.user.findUnique({
+        where: {email}
+    })
+
+    if(existingUser) {
+        return { error: "Email is already taken!"}
+    }
+
+    await db.user.create({
+        data: {
+            name, 
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    //TODO: send verification token email
 
     return {success: "Email Sent"}
 }
