be95250060d8bae9d2918f60a333cb03363e526e
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-02-24 15:40:35 -0400

    credential provider defined

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,30 @@
-import GitHub from "next-auth/providers/github"
-
+import Credentials from "next-auth/providers/credentials"
 import type { NextAuthConfig } from "next-auth"
+import { LoginSchema } from "./schemas"
+import { getUserByEmail } from "./lib/data/user"
+import bcrypt from "bcryptjs"
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials)
+
+        if(validatedFields.success) {
+          const { email, password} = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+
+          //users not having account or users registered through other providers
+          if (!user?.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if(passwordMatch) return user;
+        }
+
+        return null
+      }
+    })
+  ],
 } satisfies NextAuthConfig
