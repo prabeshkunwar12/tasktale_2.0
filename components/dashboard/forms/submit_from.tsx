2e87d71fd0c9da3ea83097ca848e27cc52f99a1a
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-03-09 23:27:00 -0400

    fixed popover dialog

--- components/dashboard/forms/submit_from.tsx
+++ components/dashboard/forms/submit_from.tsx
@@ -3,7 +3,7 @@
 import { trpc } from '@/app/_trpc/client'
 import { Button } from '@/components/ui/button'
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { cn } from '@/lib/utils'
 import { NewTaskSchema } from '@/schemas'
@@ -14,7 +14,6 @@ import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import Skeleton from 'react-loading-skeleton'
 import * as z from 'zod'
-import { subTypes } from '../constants/lists'
 
 
 
@@ -34,23 +33,21 @@ const SubmitForm = () => {
         resolver: zodResolver(NewTaskSchema),
     })
 
-
-    {console.log("subTypeList:", subTypeList)}
-
     const onSubmit = () => {
         setError("")
-        const { mutate:createTask, isPending } = trpc.createTask.useMutation({
-            onSuccess: (task) => {
-                if(task) router.push(`dashboard/tasks/${task?.id}`)
-                else setError("Task Type Not Found")
-            },
-
-            onError: () => {
-                setError("Failed to Create Task")
-                form.reset()
-            }
-        })
+        
     }
+    const { mutate:createTask, isPending } = trpc.createTask.useMutation({
+        onSuccess: (task) => {
+            if(task) router.push(`dashboard/tasks/${task?.id}`)
+            else setError("Task Type Not Found")
+        },
+
+        onError: () => {
+            setError("Failed to Create Task")
+            form.reset()
+        }
+    })
 
     if(!subTypeList) {
         return (
@@ -60,7 +57,7 @@ const SubmitForm = () => {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col space-y-4'>
+            <form onSubmit={} className='flex flex-col space-y-4'>
                 <FormField
                     control={form.control}
                     name="subTypeName"
@@ -70,14 +67,14 @@ const SubmitForm = () => {
                                 <PopoverTrigger asChild>
                                     <FormControl>
                                         <Button 
-                                            variant="outline"  
+                                            variant="outline" 
                                             className={cn
                                                 ("w-[200px] justify-between",
-                                                !field.value && "text-muted-foreground"
+                                                !field.value && "text-muted-foreground",
                                             )}
                                         >
                                             {field.value ?? "Select The Task"}
-                                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50 " />
                                         </Button>
                                     </FormControl>
                                 </PopoverTrigger>
@@ -92,7 +89,6 @@ const SubmitForm = () => {
                                                             value={subType.name}
                                                             key={subType.name}
                                                             onSelect={() => {
-                                                                console.log("Selected subType:", subType);
                                                                 form.setValue("subTypeName", subType.name);
                                                             }}
                                                         >
