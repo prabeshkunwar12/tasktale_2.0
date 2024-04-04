6a28e116e013ce41fd0c19db9ee73d190ec8ec59
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-04-04 19:42:57 -0300

    progressbar indicator implemented

--- components/profile/profile-image.tsx
+++ components/profile/profile-image.tsx
@@ -112,7 +112,7 @@ const ProfileImageDropzone = () => {
                             {isUploading ? (
                                 <div className='w-full mt-4 max-w-xs mx-auto'>
                                 <Progress
-                                    // indicatorColor={uploadProgress === 100 ? 'bg-green-500' : ''}
+                                    indicatorColor={uploadProgress === 100 ? 'bg-green-500' : ''}
                                     value={uploadProgress}
                                     className='h-1 w-full bg-zinc-200'
                                 />
--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -20,7 +20,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={cn("h-full w-full flex-1 bg-primary transition-all", indicatorColor)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
