7c7757e1eac5222e2160031b8ba1ec8c6d54872b
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-04-05 13:15:14 -0300

    ready for production, database updated

--- components/profile/profile-image.tsx
+++ components/profile/profile-image.tsx
@@ -93,7 +93,7 @@ const ProfileImageDropzone = () => {
                                 </p>
                             </div>
 
-                            {acceptedFiles && acceptedFiles[0] ? (
+                            {acceptedFiles[0] ? (
                                 <div className='max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200'>
                                     <div className='px-3 py-2 h-full grid place-items-center'>
                                         <File className='h-4 w-4 text-blue-500' />
